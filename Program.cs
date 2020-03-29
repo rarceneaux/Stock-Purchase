9b0b0fc5e497bd3fd4f68052866dd07ddbb173f6
Raymond <raymond.techcoder@gmail.com>
2020-03-29 16:33:30 -0500

    WIP

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Stock_Purchase
 {
@@ -7,7 +8,23 @@ namespace Stock_Purchase
         static void Main(string[] args)
         {
             Console.WriteLine("Too Big To Fail");
-          
+
+            Dictionary<string, string> stocks = new Dictionary<string, string>();
+            stocks.Add("GDDY", "Go Daddy");
+            stocks.Add("BA", "Boeing");
+            stocks.Add("UBER", "Uber");
+
+         List<(string ticker, int shares, double price)> buys = new List<(string, int, double)>();
+            buys.Add(("GDDY", 500, 48.21));
+            buys.Add(("BA", 500, 166.21));
+            buys.Add(("GDDY", 500, 50.21));
+
+
+            Dictionary<string, int> totalStockOwnedInfo = new Dictionary<string, int>();
+
+            stocks.
+
+
         }
     }
 }
